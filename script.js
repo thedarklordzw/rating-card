4dfdfb7f69eacb542c6dc683e6cbaf30e7d1728a
thedarklordzw <swiftly.graphics@gmail.com>
2022-06-04 13:03:05 +0200

    added more js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const btnContainer = document.querySelector('.container');
 const cards = document.querySelectorAll('.card');
+
 const cardsArr = Array.from(cards);
+const buttonsEl = document.querySelectorAll('.btn');
+const buttonsElArr = Array.from(buttonsEl);
+const numButtons = buttonsElArr.length;
 
 btnContainer.addEventListener('click', event => {
-  const buttonId = event.target.closest('.btn');
+  const buttonId = event.target.closest('.btn').innerHTML;
+
   if (!buttonId) return;
   else {
-    cards.forEach(card => {
-      card.classList.toggle('card-hidden');
-    });
-    console.log(buttonId.innerHTML);
+    const result = buttonsElArr.at(Number(buttonId) - 1);
+    buttonsElArr.forEach(element => element.classList.remove('btn--clicked'));
+    result.classList.toggle('btn--clicked');
   }
 });
